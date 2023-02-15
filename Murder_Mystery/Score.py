c13ae19a8de380ee419749c247008b08bec271a3
Marc <56824499+NessInMorse@users.noreply.github.com>
2023-02-15 14:16:34 +0100

    added for testing

--- Murder_Mystery/Score.py
+++ Murder_Mystery/Score.py
@@ -4,8 +4,7 @@ class Eindstand:
                 self.optie_menu()
 
 
-        def check_valid_input(self, menu_choice):
-                options = ["1", "2", "3", "x"]
+        def check_valid_input(self, menu_choice, options):
                 if menu_choice in options:
                         return menu_choice
                 print("\n\nJe input was niet geldig, " +\
@@ -24,23 +23,66 @@ class Eindstand:
                                                 "2) Punten invullen voor opdracht 1\n" +\
                                                 "3) Punten invullen voor opdracht 2\n" +\
                                                 "x) Eindig het script\n").lower()
-                                menu_choice = self.check_valid_input(menu_choice)
+                                menu_choice = self.check_valid_input(menu_choice, ["1", "2", "3", "x"])
                         if menu_choice == "1":
-                                print("Komt nu uit bij 1")
-                        elif menu_choice == "2":
-                                print("Komt nu uit bij 2")
-                        elif menu_choice == "3":
-                                print("Komt nu uit bij 3")
+                                self.add_group()
+                        elif menu_choice == "2" and self.groups:
+                                self.add_exercise_points(0)
+                        elif menu_choice == "3" and self.groups:
+                                self.add_exercise_points(1)
                 self.writeFile()
         
+        def check_valid_group_name(self, new_group):
+                if new_group not in self.groups:
+                        return new_group
+                return ""
+                
+
         def add_group(self):
-                self.groups
+                new_group = ""
+                while not new_group:
+                        new_group = input("\nGeef de naam van jullie groep:\n").lower()
+                        new_group = self.check_valid_group_name(new_group)
+                        if not new_group:
+                                print("\n\nHet lijkt erop dat deze groepsnaam al bestaat, probeer een andere groepsnaam te bedenken.\n\n")
+                self.groups[new_group] = [0, 0]
+
+
+
+        def add_exercise_points(self, exercise):
+                group_name = ""
+                newline = "\n"
+                while not group_name:
+                        print(f"Dit zijn de verschillende groepen:\n{f'{newline}'.join(self.groups.keys())}")
+                        group_name = input("\nGeef de naam van jullie groep:\n").lower()
+                        if group_name not in self.groups:
+                                group_name = ""
+                
+                points = 0
+                # Punten zelf
+                score_answers = int(input("Hoeveel punten heeft het groepje in totaal behaald?"))
+
+
+                # Placement
+                # [20, 15, 10, 5, 0]
+                options = [20, 15, 10, 5, 0]
+                placement = 0
+
+                # Bonus (hint kaartjes)
+                # 5 * n kaartjes
+
+
+                # total
+                points = score_answers + placement + 0
+
+
+                print("Yo lekker gedaan")
 
         def writeFile(self):
                 infile = open("groeps_punten.tsv", "w")
                 infile.write("groepsnaam\topdr1\topdr2\n")
                 for group in self.groups.keys():
-                        infile.write(f"{group}\t{self.groups[group][0]}\t{self.groups[group][1]}")
+                        infile.write(f"{group}\t{self.groups[group][0]}\t{self.groups[group][1]}\n")
                 infile.close()
                         
 
@@ -51,4 +93,3 @@ class Eindstand:
 
 if __name__ == "__main__":
         eindstand = Eindstand()
-        
